64bedbb57dfcedf09aed7730ba00e1ced7095d10
Yassar Ahmed <yassarahmed17@gmail.com>
2023-07-10 14:07:40 -0500

    adds model with seeds

---
+++ db/migrate/20230710185458_create_musics.rb
@@ -0,0 +1,12 @@
+class CreateMusics < ActiveRecord::Migration[7.0]
+  def change
+    create_table :musics do |t|
+      t.string :title
+      t.string :album
+      t.string :artist
+      t.integer :year
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Music.create(
+  title: "Let It Happen",
+  album: "Currents",
+  artist: "Tame Impala",
+  year: 2015,
+)
+
+Music.create(
+  title: "Save Your Tears",
+  album: "After Hours",
+  artist: "The Weeknd",
+  year: 2020,
+)
+Music.create(
+  title: "The Color Violet",
+  album: "Alone at Prom",
+  artist: "Tory Lanez",
+  year: 2021,
+)
